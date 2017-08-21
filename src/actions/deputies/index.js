47e434d7b358c9bcaa105744760eae34d5962ecc
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2017-08-21 10:33:43 -0300

    Initial actions for deputies

---
+++ src/actions/deputies/index.js
@@ -0,0 +1,29 @@
+import {
+  GET_DEPUTIES,
+  GET_DEPUTIES_SUCCESS,
+} from '../types';
+
+import apiClient from '../../clients/api';
+
+
+const getDeputiesSuccess = (dispatch, listOfDeputies) => {
+  dispatch({
+    type: GET_DEPUTIES_SUCCESS,
+    payload: listOfDeputies,
+  });
+};
+
+const getDeputies = () => (dispatch) => {
+  dispatch({ type: GET_DEPUTIES });
+  apiClient.get('/deputados')
+    .then(({ data }) => {
+      const { dados } = data;
+      getDeputiesSuccess(dispatch, dados);
+    })
+    .catch(err => console.log(err));
+};
+
+
+export default {
+  getDeputies,
+};
