881d1abf6468c37e5e4c23430bb59a069fc36b1d
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2017-08-21 08:38:20 -0300

    Configuring Redux on the App

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,12 +1,26 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import { StackNavigator } from 'react-navigation';
 
 import Home from '../../screens/home';
+import reducers from '../../reducers';
 
 
-const App = StackNavigator({
+const AppNavigation = StackNavigator({
   Home: { screen: Home },
 });
+
+const App = () => {
+  const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+  return (
+    <Provider store={store}>
+      <AppNavigation />
+    </Provider>
+  );
+};
 
 
 AppRegistry.registerComponent('camara_deputados', () => App);
