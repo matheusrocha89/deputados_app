f5078edc911c1c7af22992a40d81a2103af24ce8
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2017-08-21 23:44:04 -0300

    Create LoadMore on deputies list [WIP] (#3)
    Create LoadMore on deputies list

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { ScrollView } from 'react-native';
+import { FlatList, View, ActivityIndicator } from 'react-native';
 import { List, ListItem } from 'react-native-elements';
 
-import { getDeputies } from '../../actions';
+import { getDeputies, getMoreDeputies } from '../../actions';
 
+
+const loadingStyle = {
+  paddingVertical: 20,
+  borderTopWidth: 1,
+  borderTopColor: '#DDDDDD',
+};
 
 class Home extends Component {
   static navigationOptions = {
@@ -15,29 +21,54 @@ class Home extends Component {
     this.props.getDeputies();
   }
 
+  renderLoader = () => {
+    const { loading } = this.props;
+
+    if (!loading) return null;
+
+    return (
+      <View style={loadingStyle}>
+        <ActivityIndicator animating size="large" />
+      </View>
+    );
+  };
+
+  handleLoadMore = () => {
+    const { pagination, getMoreDeputies } = this.props;
+    if (pagination.next) {
+      getMoreDeputies(pagination.next);
+    }
+  }
+
   render() {
     const { listOfDeputies } = this.props;
     return (
-      <ScrollView>
-        <List>
-          {listOfDeputies.map((deputy) => (
+      <List>
+        <FlatList
+          data={listOfDeputies}
+          keyExtractor={(item) => item.id}
+          ListFooterComponent={this.renderLoader}
+          onEndReached={this.handleLoadMore}
+          onEndReachedThreshold={1}
+          renderItem={({ item }) => (
             <ListItem
               roundAvatar
-              avatar={{uri: deputy.urlFoto.replace('http', 'https')}}
-              key={deputy.id}
-              title={deputy.nome}
-              subtitle={deputy.siglaPartido}
+              avatar={{ uri: item.urlFoto.replace('http', 'https')}}
+              title={item.nome}
+              subtitle={item.siglaPartido}
             />
-          ))}
-        </List>
-      </ScrollView>
+          )}
+        />
+      </List>
     );
   }
 }
 
-const mapStateToProps = ({ deputies: { listOfDeputies} }) => ({
-  listOfDeputies
+const mapStateToProps = ({ deputies: { listOfDeputies, pagination, loading } }) => ({
+  listOfDeputies,
+  pagination,
+  loading,
 });
 
 
-export default connect(mapStateToProps, { getDeputies })(Home);
+export default connect(mapStateToProps, { getDeputies, getMoreDeputies })(Home);
