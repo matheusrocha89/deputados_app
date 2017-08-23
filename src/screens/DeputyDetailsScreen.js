b98aac4537c089e2b05b0b23e66327c8a7e5ec5b
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2017-08-23 10:02:15 -0300

    Get the Deputy details data on DeputyDetailScreen
    Just get, not showing yet.

--- src/screens/DeputyDetailsScreen.js
+++ src/screens/DeputyDetailsScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { StyleSheet, Text, View } from 'react-native';
@@ -29,50 +29,59 @@ const styles = StyleSheet.create({
   },
 });
 
-const DeputyDetailsScreen = (props) => {
-  const { deputy } = props.navigation.state.params;
-  const { goBack } = props.navigation;
-  return (
-    <View>
+
+class DeputyDetailsScreen extends Component {
+  static propTypes = {
+    navigation: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+    getDeputyDetails: PropTypes.func.isRequired,
+  };
+  static defaultProps = {
+    navigation: {},
+  };
+
+  componentDidMount() {
+    const { id } = this.props.navigation.state.params.deputy;
+    this.props.getDeputyDetails(id);
+  }
+
+  render() {
+    const { deputy } = this.props.navigation.state.params;
+    const { goBack } = this.props.navigation;
+    return (
       <View>
-        <View style={styles.headerContainer}>
-          <View style={{ flex: 1 }}>
-            <Button
-              raised
-              icon={{ name: 'arrow-back' }}
-              title="Voltar"
-              onPress={() => goBack()}
-              style={{ margin: 5 }}
-            />
+        <View>
+          <View style={styles.headerContainer}>
+            <View style={{ flex: 1 }}>
+              <Button
+                raised
+                icon={{ name: 'arrow-back' }}
+                title="Voltar"
+                onPress={() => goBack()}
+                style={{ margin: 5 }}
+              />
+            </View>
           </View>
         </View>
+        <View style={styles.bodyContainer}>
+          <Avatar
+            xlarge
+            rounded
+            source={{ uri: deputy.urlFoto.replace('http', 'https') }}
+            activeOpacity={0.7}
+          />
+          <Text style={styles.name}>{deputy.nome}</Text>
+          <Text style={styles.politicalParty}>{`${deputy.siglaPartido}-${deputy.siglaUf}`}</Text>
+          <Text style={styles.situation}>Situação: Exercício</Text>
+        </View>
       </View>
-      <View style={styles.bodyContainer}>
-        <Avatar
-          xlarge
-          rounded
-          source={{ uri: deputy.urlFoto.replace('http', 'https') }}
-          activeOpacity={0.7}
-        />
-        <Text style={styles.name}>{deputy.nome}</Text>
-        <Text style={styles.politicalParty}>{`${deputy.siglaPartido}-${deputy.siglaUf}`}</Text>
-        <Text style={styles.situation}>Situação: Exercício</Text>
-      </View>
-    </View>
-  );
-};
-
-DeputyDetailsScreen.propTypes = {
-  navigation: PropTypes.object, // eslint-disable-line react/forbid-prop-types
-};
-
-DeputyDetailsScreen.defaultProps = {
-  navigation: {},
-};
+    );
+  }
+}
 
 const mapStateToProps = ({ deputies: { currentDeputy, loading } }) => ({
   currentDeputy,
   loading,
 });
+
 
 export default connect(mapStateToProps, { getDeputyDetails })(DeputyDetailsScreen);
