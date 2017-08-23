9d22a6cfc9b556432c24b37a6bc88a8e1a07abe7
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2017-08-23 13:37:42 -0300

    Small code improvement

--- src/reducers/deputies/index.js
+++ src/reducers/deputies/index.js
@@ -18,10 +18,7 @@ const INITIAL_STATE = {
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case GET_DEPUTIES || GET_DEPUTY_DETAILS:
-      return { ...state, loading: true };
-
-    case GET_MORE_DEPUTIES:
+    case GET_DEPUTIES || GET_DEPUTY_DETAILS || GET_MORE_DEPUTIES:
       return { ...state, loading: true };
 
     case GET_DEPUTIES_SUCCESS:
