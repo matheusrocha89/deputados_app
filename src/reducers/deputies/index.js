68fc11f2eaf2273b57ad108ca4e4ac49f515e4ce
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2017-08-23 09:44:28 -0300

    Create the reducer to treat the get of deputy details

--- src/reducers/deputies/index.js
+++ src/reducers/deputies/index.js
@@ -3,6 +3,8 @@ import {
   GET_MORE_DEPUTIES,
   GET_DEPUTIES_SUCCESS,
   GET_MORE_DEPUTIES_SUCCESS,
+  GET_DEPUTY_DETAILS,
+  GET_DEPUTY_DETAILS_SUCCESS,
 } from '../../actions/types';
 
 
@@ -16,22 +18,32 @@ const INITIAL_STATE = {
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case GET_DEPUTIES:
+    case GET_DEPUTIES || GET_DEPUTY_DETAILS:
       return { ...state, loading: true };
+
     case GET_MORE_DEPUTIES:
       return { ...state, loading: true };
+
     case GET_DEPUTIES_SUCCESS:
       return {
         ...state,
         ...action.payload,
         loading: false,
       };
+
     case GET_MORE_DEPUTIES_SUCCESS:
       return {
         ...state,
         listOfDeputies: [...state.listOfDeputies, ...action.payload.listOfDeputies],
         pagination: action.payload.pagination,
         loading: false,
+      };
+
+    case GET_DEPUTY_DETAILS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        currentDeputy: action.payload,
       };
     default:
       return state;
