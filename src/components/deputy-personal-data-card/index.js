2e1bcd7c1849128b1821a0925d0628c91f010dcd
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2017-08-24 21:13:12 -0300

    Improve personal data

--- src/components/deputy-personal-data-card/index.js
+++ src/components/deputy-personal-data-card/index.js
@@ -5,12 +5,15 @@ import { Card } from 'react-native-elements';
 import isEmpty from 'lodash/isEmpty';
 import moment from 'moment';
 
+import { getStateByInitials } from '../../utils/brazil-states';
+
 
 const DeputyPersonalDataCard = ({ deputy }) => {
   if (isEmpty(deputy)) return null;
 
   const { ultimoStatus } = deputy;
   const birthDate = moment(deputy.dataNascimento).format('DD/MM/YYYY');
+  const state = getStateByInitials(deputy.ufNascimento) || deputy.ufNascimento;
   return (
     <Card title="Dados Pessoais">
       <Text>Nome Civil: {deputy.nomeCivil}</Text>
@@ -20,7 +23,12 @@ const DeputyPersonalDataCard = ({ deputy }) => {
         <Text> CPF: {deputy.cpf}</Text>
       ) : null}
       <Text>Data Nascimento: {birthDate}</Text>
+      {deputy.dataFalecimento ? (
+        <Text>Data de falecimento: {deputy.dataFalecimento}</Text>
+      ) : null}
       <Text>Escolaridade: {deputy.escolaridade ? deputy.escolaridade : 'Não informada'}</Text>
+      <Text>Estado de Nascimento: {state}</Text>
+      <Text>Cidade de Nascimento: {deputy.municipioNascimento}</Text>
     </Card>
   );
 };
