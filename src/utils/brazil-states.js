80803aed298108dcd43598393d1520386625d854
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2017-08-24 21:13:02 -0300

    Create utils for state

---
+++ src/utils/brazil-states.js
@@ -0,0 +1,38 @@
+const states = {
+  AC: 'Acre',
+  AL: 'Alagoas',
+  AP: 'Amapá',
+  AM: 'Amazonas',
+  BA: 'Bahia',
+  CE: 'Ceará',
+  DF: 'Distrito Federal',
+  ES: 'Espírito Santo',
+  GO: 'Goiás',
+  MA: 'Maranhão',
+  MT: 'Mato Grosso',
+  MS: 'Mato Grosso do Sul',
+  MG: 'Minas Gerais',
+  PA: 'Pará',
+  PB: 'Paraíba',
+  PR: 'Paraná',
+  PE: 'Pernambuco',
+  PI: 'Piauí',
+  RJ: 'Rio de Janeiro',
+  RN: 'Rio Grande do Norte',
+  RS: 'Rio Grande do Sul',
+  RO: 'Rondônia',
+  RR: 'Roraima',
+  SC: 'Santa Catarina',
+  SP: 'São Paulo',
+  SE: 'Sergipe',
+  TO: 'Tocantins',
+};
+
+export function getStateByInitials(initial) {
+  return states[initial] || null;
+}
+
+export function getInitialByState(stateName) {
+  return Object.entries(states)
+    .find(state => state[1] === stateName) || null;
+}
